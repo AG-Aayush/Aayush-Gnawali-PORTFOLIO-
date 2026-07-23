f8c02b154316ba44178274d22f3323a7403e1103
Aayush Gnawali <AG-Aayush>
2026-07-23 17:48:28 +0545

    updates color theme that will blend right into the top gradient

--- portfolio/src/app/robots.ts
+++ portfolio/src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://aayushgnawali.dev/sitemap.xml",
+    sitemap: "https://www.aayushgnawali.com.np/sitemap.xml",
   };
 }
